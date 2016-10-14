62e8fd6147480d9cb5eac13f5a4dc04c2033833a
Max <thermonuclear@sbcglobal.net>
2016-10-13 18:42:09 -0700

    Adding missing files from last commit

---
+++ Source/MaxBootstrap.Core/DelegateCommand.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace MaxBootstrap.UI
+{
+    public class DelegateCommand : ICommand
+    {
+        private readonly Predicate<object> canExecute;
+        private readonly Action execute;
+
+        public void Execute(object parameter)
+        {
+            this.execute();
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public DelegateCommand(Action execute)
+                       : this(execute, null)
+        {
+        }
+
+        public DelegateCommand(Action execute,
+                       Predicate<object> canExecute)
+        {
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            if (canExecute == null)
+            {
+                return true;
+            }
+
+            return canExecute(parameter);
+        }
+
+        public void RaiseCanExecuteChanged()
+        {
+            if (this.CanExecuteChanged != null)
+            {
+                this.CanExecuteChanged(this, EventArgs.Empty);
+            }
+        }
+    }
+}
---
+++ Source/MaxBootstrap.UI/Pages/CancelPage.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+using MaxBootstrap.UI.Viewmodels.Concretes;
+using MaxBootstrap.UI.Views;
+
+namespace MaxBootstrap.UI.Pages
+{
+    using Core;
+    using Core.Pages;
+
+    public class CancelPage : PageBase
+    {
+        protected CancelView CancelView;
+
+        public CancelPage(IBootstrapperController bootstrapperController) : base(bootstrapperController)
+        {
+            this.CancelView = new CancelView(new CancelViewmodel(bootstrapperController));
+        }
+
+        public override FrameworkElement ViewContent
+        {
+            get
+            {
+                return this.CancelView;
+            }
+        }
+    }
+}
